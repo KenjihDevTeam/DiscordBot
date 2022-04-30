4c9fd6d5feb1fef00190615e4c208ad643e41cd1
Qreepex <65496825+Qreepex@users.noreply.github.com>
2022-04-30 13:31:45 +0200

    log event example

--- src/classes/Event.ts
+++ src/classes/Event.ts
@@ -1,3 +1,4 @@
+import { MessageEmbed } from "discord.js";
 import Bot from "./Bot";
 
 export default class Event {
@@ -10,5 +11,16 @@ export default class Event {
 
   async run(client: Bot, arg1: any, arg2: any, arg3: any): Promise<any> {
     client.Logger.warn("Base Event");
+  }
+
+  get embed(): MessageEmbed {
+    const embed = new MessageEmbed().setTimestamp().setFooter({
+      text: "Kenjih",
+      iconURL:
+        "https://cdn.discordapp.com/icons/445308722168987648/0562fc49b47c5200ba67236ab61efcd6.webp?size=256",
+    });
+
+    embed.color = 4605931;
+    return embed;
   }
 }
